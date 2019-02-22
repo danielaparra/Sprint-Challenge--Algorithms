f2b1af9fbe5de12353c9ff5ab42fb955ca933be6
Daniela Parra <dyparra47@gmail.com>
2019-02-22 12:48:47 -0500

    Robot sort implemented

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,12 +96,48 @@ class SortingRobot:
         """
         return self._light == "ON"
 
+    def check_order(self):
+        while self.can_move_right():
+            self.swap_item()
+            self.move_right()
+            if self.compare_item() == 1:
+                self.move_left()
+                self.swap_item()
+                return False
+            else:
+                self.move_left()
+                self.swap_item()
+                self.move_right()
+
+        return True
+
     def sort(self):
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+
+        while self.can_move_right():
+            self.swap_item()
+            self.move_right()
+            if self.compare_item() == 1:
+                self.swap_item()
+                self.move_left()
+                self.swap_item()
+                self.move_right()
+            else:
+                self.move_left()
+                self.swap_item()
+                self.move_right()
+
+        while self.can_move_left():
+            self.move_left()
+
+        if not self.check_order():
+            self.sort()
+
+        
+        
+
 
 
 if __name__ == "__main__":
